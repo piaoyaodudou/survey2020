14482b94742bd0d76f64fac05855fafedc288355
moxiaohui <xhmo@mobvoi.com>
2020-01-20 11:43:26 +0800

    多线程NSThread

--- 02.多线程/02.多线程/MONSThread.m
+++ 02.多线程/02.多线程/MONSThread.m
@@ -12,7 +12,11 @@
 @property (nonatomic, assign) NSInteger totalTicketCount;
 @end
 
-@implementation MONSThread
+@implementation MONSThread {
+  NSThread *_thread1;
+  NSThread *_thread2;
+  NSThread *_thread3;
+}
 
 + (instancetype)shareInstance {
   static MONSThread *instance = nil;
@@ -39,29 +43,27 @@
 
 #pragma mark - 多窗口买票
 - (void)MultiWindowTicket {
-  self.totalTicketCount = 20;
-  NSThread *thread1 = [[NSThread alloc] initWithTarget:self selector:@selector(saleTicket) object:nil];
-  thread1.name = @"窗口1";
-  NSThread *thread2 = [[NSThread alloc] initWithTarget:self selector:@selector(saleTicket) object:nil];
-  thread2.name = @"窗口2";
-  NSThread *thread3 = [[NSThread alloc] initWithTarget:self selector:@selector(saleTicket) object:nil];
-  thread3.name = @"窗口3";
-
-  [thread1 start];
-  [thread2 start];
-  [thread3 start];
+  self.totalTicketCount = 8;
+  _thread1 = [[NSThread alloc] initWithTarget:self selector:@selector(saleTicket) object:nil];
+  _thread1.name = @"窗口1";
+  _thread2 = [[NSThread alloc] initWithTarget:self selector:@selector(saleTicket) object:nil];
+  _thread2.name = @"窗口2";
+  _thread3 = [[NSThread alloc] initWithTarget:self selector:@selector(saleTicket) object:nil];
+  _thread3.name = @"窗口3";
+  [_thread1 start];
+  [_thread2 start];
+  [_thread3 start];
 }
 
 - (void)saleTicket {
-  while (YES) {
-    // swift 用 objc_sync_enter(self) 和 objc_sync_exit(self)
-//    @synchronized (self) {
-      NSInteger currentCount = self.totalTicketCount;
+  while (YES) { // 模拟还有票会持续`-1`的操作
+//    @synchronized (self) { // 互斥锁：swift 用 objc_sync_enter(self) 和 objc_sync_exit(self)
       if (self.totalTicketCount > 0) {
-        self.totalTicketCount = currentCount - 1;
+        self.totalTicketCount--;
         NSLog(@"买了一张，还剩：%ld %@", (long)self.totalTicketCount, [NSThread currentThread]);
+        [NSThread sleepForTimeInterval:0.5];
       } else {
-        NSLog(@"票买完了");
+        NSLog(@"票买完了 %@", [NSThread currentThread]);
         break;
       }
 //    }
@@ -71,30 +73,73 @@
 
 - (void)explain {
   // NSThread 是iOS中轻量级得多线程，一个NSThread对象对应一条线程
-  // 优点：操作简单
-  // 缺点：性能低，效率不高
+
+  // 一些类方法：
+//  [NSThread mainThread]; // 获取主线程
+//  [NSThread currentThread]; // 获取当前线程
+  // 阻塞当前线程，设置休眠时间，两种方式实现：
+//  [NSThread sleepForTimeInterval:3];
+//  [NSThread sleepUntilDate:[NSDate dateWithTimeIntervalSinceNow:3]];
+//  [NSThread exit]; // 立即终止主线程之外的所有线程(包括正在执行任务的)
+  // 注意：需要在掌控所有线程状态的情况下调用此方法，否则可能会导致内存问题。
+  // threadPriority相关的都已禁用，改用qualityOfService(枚举)代替
+//  [NSThread threadPriority]; // 获取当前线程优先级
+//  [NSThread setThreadPriority:0.5]; // 设置优先级：0.0~1.0；1.0优先级最高
   
   // 创建方式
   // 方法1：alloc init创建，但是需要手动开启
   NSThread *thread = [[NSThread alloc] initWithTarget:self selector:@selector(network:) object:@{@"name":@"moxiaohui"}];
-  [thread setName:@"moxiaoyan"]; // 线程名称
-//    thread.threadPriority = 1; // 线程优先级：0.1~1.0之间，默认0.5
-  // 实际上threadPriority已经过时了，需要使用qualityOfService替代。
-  thread.qualityOfService = NSQualityOfServiceUserInteractive;
-  thread.stackSize = 4096; // 更改堆栈的大小: 必须 是4KB的倍数 && 启动线程之前设置 (创建线程是会有开销的)
   [thread start];
+
+  [thread setName:@"moxiaoyan"]; // 线程名称
+  thread.qualityOfService = NSQualityOfServiceUserInteractive;
+  //  NSQualityOfServiceUserInteractive = 0x21, // 最高优先级, 用于处理 UI 相关的任务
+  //  NSQualityOfServiceUserInitiated = 0x19, // 次高优先级, 用于执行需要立即返回的任务
+  //  NSQualityOfServiceUtility = 0x11, // 普通优先级，主要用于不需要立即返回的任务
+  //  NSQualityOfServiceBackground = 0x09, // 后台优先级，用于处理一些用户不会感知的任务
+  //  NSQualityOfServiceDefault = -1 // 默认优先级，当没有设置优先级的时候，线程默认优先级
+  thread.stackSize = 8192; // 更改堆栈的大小: 必须 是4KB(1024)的倍数 && 启动线程之前设置 (创建线程是会有开销的)
+  NSUInteger size = thread.stackSize / 1024; // 所占内存大小
+  [thread cancel]; // 不会马上退出，做了需要退出的标记
+  [thread isMainThread];  // 是否是主线程
+  [thread isFinished];  // 是否已经完成
+  [thread isCancelled]; // 是否已经取消
+  [thread isExecuting]; // 是否正在执行中
   
   // 方法2：初始化一个子线程，特点：自动开启，是类方法
   @autoreleasepool {
     [NSThread detachNewThreadSelector:@selector(network:) toTarget:self withObject:@{@"name":@"moxiaohui"}];
   }
+  // 方法3：隐式创建
+  // 子线程中执行：(耗时操作)
+  [self performSelectorInBackground:@selector(network:) withObject:@{@"name":@"moxiaohui"}];
+  // 主线程中执行：(执行更新UI之类得操作)
+  [self performSelectorOnMainThread:@selector(complete) withObject:nil waitUntilDone:YES];
+  // 指定线程中执行
+  [self performSelector:@selector(complete) onThread:[NSThread mainThread] withObject:@{@"name":@"moxiaohui"} waitUntilDone:YES];
+  // 当前线程中执行
+  [self performSelector:@selector(network:) withObject:@{@"name":@"moxiaohui"}];
+  [self performSelector:@selector(network:) withObject:@{@"name":@"moxiaoyan"} withObject:@{@"name":@"moxiaohui"}];
+  [self performSelector:@selector(afterDelay:) withObject:@{@"name":@"moxiaoyan"} afterDelay:5]; // 5s后执行
+  // cancel 某一个方法
+  [NSObject cancelPreviousPerformRequestsWithTarget:self selector:@selector(afterDelay:) object:@{@"name":@"moxiaoyan"}];
+  // cancel 当前对象所有perform方法
+  [NSObject cancelPreviousPerformRequestsWithTarget:self];
+}
+
+- (void)afterDelay:(NSDictionary *)info {
+  NSLog(@"afterDelay info:%@", info);
 }
 
 - (void)network:(NSDictionary *)info {
-  NSLog(@"%@", [NSThread currentThread]);
+  NSLog(@"执行 %@", [NSThread currentThread]);
   NSLog(@"info: %@", info);
-  sleep(2); // 模拟耗时操作
-  [[NSThread currentThread] cancel];
+  sleep(2);
+  NSLog(@"完成");
+}
+
+- (void)complete {
+  NSLog(@"Update UI");
 }
 
 @end
--- 02.多线程/02.多线程/ViewController.m
+++ 02.多线程/02.多线程/ViewController.m
@@ -17,6 +17,19 @@
 @implementation ViewController
 - (void)viewDidLoad {
   [super viewDidLoad];
+  // GCD：C，生命周期：自动管理
+  // 优点：线程安全；跟block结合，代码简洁；更接近底层，高效；
+  // 缺点：基于C实现
+  
+  // NSOperation：OC，生命周期：自动管理
+  // 优点：线程安全；比GCD多了一些简单实用得功能(比如：优先顺序、结合queue使用可随时取消已经设定要执行得任务); 提高了代码的复用度
+  // 缺点：基于GCD封装，更高级，抽象；更加面向对象；
+  
+  // NSThread：OC，生命周期：程序猿管理
+  // 优点：比前两种更轻量级；可以睡眠和唤醒；简单易用可直接操作线程对象
+  // 缺点：非线程安全，需要加锁，性能低；需要管理生命周期；
+  // 底层是Pthread，基于C实现
+  
 //  [MOGCD shareInstance];
 //  [MOOperationQueue shareInstance];
   [MONSThread shareInstance];
