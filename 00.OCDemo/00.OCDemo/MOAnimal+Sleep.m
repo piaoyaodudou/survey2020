e82afe32816411f8030d481b0d53e0df8da30523
xhmo <xhmo@mobvoi.com>
2020-07-15 23:05:42 +0800

    category、extension 探讨2

--- 00.OCDemo/00.OCDemo/MOAnimal+Sleep.m
+++ 00.OCDemo/00.OCDemo/MOAnimal+Sleep.m
@@ -9,22 +9,29 @@
 #import "MOAnimal+Sleep.h"
 #import <objc/runtime.h>
 
+static NSString *placeKey = @"place";
+static NSString *sleepDlegateKey = @"sleepDelegate";
+
+// 1. static char studentNameKey; // 固定地址
+// 2. static const void *studentNameKey = &studentNameKey;
+// 3. @selector(place) // 使用getter方法名作为key
+
 @implementation MOAnimal (Sleep)
 
 // 需要在.m文件 #import <objc/runtime.h>
 // 然后用runtime实现set get 方法
 - (void)setPlace:(NSString *)place {
-  objc_setAssociatedObject(self, @"place", place, OBJC_ASSOCIATION_COPY);
+  objc_setAssociatedObject(self, &placeKey, place, OBJC_ASSOCIATION_COPY);
 }
 - (NSString *)place {
-  return objc_getAssociatedObject(self, @"place");
+  return objc_getAssociatedObject(self, &placeKey);
 }
 
 - (void)setSleepDelegate:(id<MOAnimalSleepDelegate>)sleepDelegate {
-  objc_setAssociatedObject(self, @"sleepDelegate", sleepDelegate, OBJC_ASSOCIATION_ASSIGN);
+  objc_setAssociatedObject(self, &sleepDlegateKey, sleepDelegate, OBJC_ASSOCIATION_ASSIGN);
 }
 - (id<MOAnimalSleepDelegate>)sleepDelegate {
-  return objc_getAssociatedObject(self, @"sleepDelegate");
+  return objc_getAssociatedObject(self, &sleepDlegateKey);
 }
 
 - (void)sleep {
